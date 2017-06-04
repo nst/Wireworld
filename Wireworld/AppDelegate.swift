c9efde6f7ead9d9e5da6b981729d26a35d5fd028
Nicolas Seriot <nicolas@seriot.ch>
2017-06-04 20:22:13 +0200

    preparing release 0.1

--- Wireworld/AppDelegate.swift
+++ Wireworld/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        checkForUpdates()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -27,5 +28,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func saveImageAction(sender: NSMenuItem?) {
         guard let document : Document = NSApplication.shared().mainWindow?.windowController?.document as? Document else { return }
         document.saveImageAction(sender: sender)
+    }
+    
+    func checkForUpdates() {
+        
+        let url = URL(string:"http://www.seriot.ch/wireworld/wireworld.json")
+        
+        URLSession.shared.dataTask(with: url!) { (optionalData, response, error) in
+            
+            DispatchQueue.main.async {
+                
+                guard let data = optionalData,
+                    let optionalDict = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:AnyObject],
+                    let d = optionalDict,
+                    let latestVersionString = d["latest_version_string"] as? String,
+                    let latestVersionURL = d["latest_version_url"] as? String
+                    else {
+                        return
+                }
+                
+                print("-- latestVersionString: \(latestVersionString)")
+                print("-- latestVersionURL: \(latestVersionURL)")
+                
+                guard let currentVersionString = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else { return }
+                
+                let needsUpdate = currentVersionString < latestVersionString
+                
+                print("-- needsUpdate: \(needsUpdate)")
+                if needsUpdate == false { return }
+                
+                let a = NSAlert()
+                a.messageText = "ECAExplorer \(latestVersionString) is Available"
+                a.informativeText = "Please download it and replace the current version.";
+                a.addButton(withTitle: "Download")
+                a.addButton(withTitle: "Cancel")
+                a.alertStyle = .critical
+                
+                let modalResponse = a.runModal()
+                
+                if modalResponse == NSAlertFirstButtonReturn {
+                    if let downloadURL = URL(string:latestVersionURL) {
+                        NSWorkspace.shared().open(downloadURL)
+                    }
+                }
+            }
+            }.resume()
     }
 }
